76036043ce8b5f5e9b4c32a73975aae28bec5ef9
Jatin Aggarwal <jatin.aggarwal@freechargebiz.com>
2026-03-29 15:43:01 +0530

    Creating nested div fucntion in App.js

--- App.js
+++ App.js
@@ -1,3 +1,36 @@
+    /**
+     * 
+     * How to create a below srtructure in React
+     * <div id="parent">
+     *   <div id="child">   
+     *       <h1>I am hi tag</h1>
+     *   </div>   
+     * </div>
+     */
+    
+
+    
+    
+    
+    
     const heading =React.createElement('h1', {id:"heading"}, 'Hello World from React from diff file');
+    console.log(heading);
+    
     const root = ReactDOM.createRoot(document.getElementById('helloWorld'));
-    root.render(heading);
+    // root.render(heading);
+
+    const parent = React.createElement(
+        "div",
+        {id:"parent"},
+        React.createElement(
+            "div",
+            {id:"child"},
+            React.createElement(
+                "h1",
+                {},
+                "I am hi tag"
+            )
+        )
+    )
+
+    root.render(parent);
