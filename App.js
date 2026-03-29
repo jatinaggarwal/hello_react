accf138771d48e06301f3cc29803cd845f469b8b
Jatin Aggarwal <jatin.aggarwal@freechargebiz.com>
2026-03-29 15:54:54 +0530

    creating sibiling with more complex structure

--- App.js
+++ App.js
@@ -20,7 +20,20 @@
      * </div>
      */
     
-    
+          /**
+     * 
+     * How to create a below srtructure in React
+     * <div id="parent">
+     *   <div id="child">   
+     *       <h1>I am hi tag</h1>
+     *      <h2>I am h2 tag</h2>
+     *   </div>   
+     * <div id="child2">   
+     *       <h1>I am hi tag</h1>
+     *      <h2>I am h2 tag</h2>
+     *   </div> 
+     * </div>
+     */
     
     const heading =React.createElement('h1', {id:"heading"}, 'Hello World from React from diff file');
     console.log(heading);
@@ -31,7 +44,7 @@
     const parent = React.createElement(
         "div",
         {id:"parent"},
-        React.createElement(
+        [React.createElement(
             "div",
             {id:"child"},
             [React.createElement(
@@ -43,7 +56,19 @@
                 {},
                 "I am h2 tag"
             )]
-        )
+        ),React.createElement(
+            "div",
+            {id:"child2"},
+            [React.createElement(
+                "h1",
+                {},
+                "I am hi tag"
+            ),React.createElement(
+                "h2",
+                {},
+                "I am h2 tag"
+            )]
+        )]
     )
 
     root.render(parent);
